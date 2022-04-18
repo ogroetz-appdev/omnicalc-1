1a9c2b5daf9548ac456931d28df595cdcc0eacbe
osha7 <ogroetz@gmail.com>
2022-04-18 04:35:26 +0000

    square results - not fully working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   def blank_square_form
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
+  
+  def square_results
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
 
 
 end
